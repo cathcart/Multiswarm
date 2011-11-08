2e0fc03408461a512221a0ef2a904b64aeaaeda9
Thomas Cathcart <thomas.cathcart+git@gmail.com>
2011-11-08 13:45:19 +0000

    setup

---
+++ server.py
@@ -0,0 +1,12 @@
+import Pyro4
+import Queue
+
+class server():
+	def __init__():
+		self.jobs = Queue.queue()
+		self.results = Queue.queue()
+		self.no_clients = 0
+
+
+if __name__ == "__main__":
+	print "setup server"		
